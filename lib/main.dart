eb86432c55206347ea1eba322d18190b73f13e16
Brandon O'Douglas <brandondouglas247@gmail.com>
2025-03-24 18:47:23 +0000

    TESTING

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,63 @@
 import 'package:flutter/material.dart';
 
+// Use this to make text styles to text specific text widget to make styles/colouring more uniform
+
 void main() => runApp(MaterialApp(home: BrandonCard()));
 
 class BrandonCard extends StatefulWidget {
   @override
   State<BrandonCard> createState() => _BrandonCardState();
 }
+
 class _BrandonCardState extends State<BrandonCard> {
   // Data here can now change over time thanks to stateful widget
   // We then need to replace the widget in question with $myCounter
   int myCounter = 0;
+  // List of todos
+  List<String> todoList = [];
+
+  // for the text field in the todos
+  final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState((){
+          setState(() {
             myCounter += 1;
           });
         },
         child: Text(
           'INCREMENT',
           textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.white, fontSize:8 ),
+          style: TextStyle(color: Colors.white, fontSize: 8),
           maxLines: 1,
         ),
-        backgroundColor: Colors.red,
+        backgroundColor: const Color.fromARGB(255, 103, 216, 22),
       ),
       // Red AppBar with my name (centered) on it
+      // Need to figure out how to center a title on navbar with hamburger icon from nav bar
       appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Brandon Douglas!',
-            style: TextStyle(color: Colors.white),
+        // Centers the title properly in the app bar without center widget
+        centerTitle: true,
+        title: Text(
+          'S I M P L E - S A L A H.',
+          style: TextStyle(
+            color: const Color.fromARGB(255, 255, 255, 255),
+            fontSize: 20,
+            fontFamily: "Helvetica Light Bold",
+            fontWeight: FontWeight.w100,
           ),
+          textAlign: TextAlign.center,
         ),
-        backgroundColor: Colors.red,
+
+        //centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 103, 216, 22),
+        iconTheme: IconThemeData(color: Colors.white),
+
+        // Increase toolbar height so that the title text fits within the app
       ),
+
       body: Center(
         child: Column(
           children: [
@@ -48,23 +69,80 @@ class _BrandonCardState extends State<BrandonCard> {
             Container(
               child: Column(
                 children: [
-                  Text('Hey, Welcome to my Android App.'),
-                  Text('I hope you enjoy your stay!'),
-                  Text('- Brandon.'),
+                    
+                  Text(
+                    'As-salamu alaykum',
+                    style: TextStyle(
+                      fontFamily: "Helvetica Light Bold",
+                      fontWeight: FontWeight.w100,
+                    ),
+                  ),
+                   Padding(padding:  EdgeInsets.all(10.0),
+                  child: Divider(
+                    height: 0,
+                    thickness: 0.8,
+                    indent: 0,
+                    endIndent: 0,
+
+                    color: const Color.fromARGB(255, 192, 187, 187),
+                    
+                  ),),
+                  Center(
+                    child: Text(
+                      'Made for reverts, by a revert.',
+                    ),
+                  ),
+                  Text(
+                    'Click the buttons in the bottom drawer to begin.',
+                    textAlign: TextAlign.center,
+                  ),
                 ],
               ),
             ),
             Container(
-              child: Text(
-                'You have pressed the button this amount of times',
-                style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+              child: Column(
+                children: [
+                  Text(
+                    'Enjoy inshallah,',
+                    style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                  Text(
+                    '- Brandon.',
+                    style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                  Text(
+                    'Islamic Prayer Time Table',
+                    style: TextStyle(
+                      fontFamily: "Helvetica Light Bold",
+                      fontWeight: FontWeight.w100,
+                      fontSize: 25,
+                    ),
+                  ),
+                  
+                  Padding(padding:  EdgeInsets.all(16.0),
+                  child: Divider(
+                    height: 0,
+                    thickness: 0.8,
+                    indent: 0,
+                    endIndent: 0,
+
+                    color: const Color.fromARGB(255, 192, 187, 187),
+                    
+                  ),),
+                  
+                  Text('$myCounter', style: TextStyle(fontSize: 100)),
+                  Center(
+                    child: Text(
+                      'Use the navigation drawer to move to a new screen!',
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text('$myCounter', style: TextStyle(fontSize: 100)),
           ],
         ),
       ),
-      backgroundColor: const Color.fromARGB(255, 222, 217, 189),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
     );
   }
 }
